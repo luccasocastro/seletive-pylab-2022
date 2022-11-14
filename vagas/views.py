968db66a3eec99cf35119d23dfe659625d1b8cc5
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-11-14 10:01:51 -0300

    criando html de vaga e css

--- vagas/views.py
+++ vagas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from empresa.models import Vagas
 from django.contrib import messages
@@ -35,3 +35,7 @@ def nova_vaga(request):
 
     elif request.method == 'GET':
         raise Http404()
+
+def vaga(request, id):
+    vaga = get_object_or_404(Vagas, id=id)
+    return render(request, 'vaga.html', {'vaga':vaga})
